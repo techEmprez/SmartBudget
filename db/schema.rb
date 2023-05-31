1f77ffd7c5d674fb0c9251021edf25eb93c395e7
Choice Osobor <choiceosobor@gmail.com>
2023-05-31 01:57:44 +0100

    fix linters errors

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_21_144250) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
+ActiveRecord::Schema[7.0].define(version: 2023_05_30_235500) do
   create_table "group_operations", force: :cascade do |t|
-    t.bigint "group_id", null: false
-    t.bigint "operation_id", null: false
+    t.integer "group_id", null: false
+    t.integer "operation_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["group_id"], name: "index_group_operations_on_group_id"
@@ -28,7 +25,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_21_144250) do
     t.string "icon"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
+    t.integer "user_id", null: false
     t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
@@ -37,7 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_21_144250) do
     t.decimal "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
+    t.integer "user_id", null: false
     t.index ["user_id"], name: "index_operations_on_user_id"
   end
 
